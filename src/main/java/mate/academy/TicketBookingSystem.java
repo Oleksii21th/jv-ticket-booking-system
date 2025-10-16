b8dd9ec9d39730992aa68cf5f7bdf3ae1f7a098e
Admin <admin@>
2025-10-16 16:38:49 +0200

    commit task

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,24 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
 
-    public TicketBookingSystem(int totalSeats) {
+    private final Semaphore seats;
 
+    public TicketBookingSystem(int totalSeats) {
+        this.seats = new Semaphore(totalSeats, true);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        if (seats.tryAcquire()) {
+            try {
+                return new BookingResult(user, true, "Booking succeed");
+            } finally {
+
+            }
+        } else {
+            return new BookingResult(user, false, "No available seats");
+        }
     }
 }
